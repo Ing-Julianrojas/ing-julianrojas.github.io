1c69d93c67b7a92c7c8e8d2f1839476bdddc14ae
Fuckkjulius <julis0293@gmail.com>
2026-07-01 15:11:02 -0500

    Update portfolio design and add CV files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://Fuckkjulius.github.io'
+  site: 'https://Fuckkjulius.github.io',
+  vite: {
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url),
+      },
+    },
+  },
 })
